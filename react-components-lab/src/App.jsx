b538febe39455184dea4bfa5b1a56b1af921fdd4
DtxLcl.19 <bryan.dtx19@icloud.com>
2025-08-12 23:22:14 -0500

    bug fix

--- react-components-lab/src/App.jsx
+++ react-components-lab/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WeatherForecast from './components/WeatherForecast/WeatherForecast'
+import WeatherForecast from './WeatherForecast/WeatherForecast.jsx'
 
 const weatherForecasts = [
   {
